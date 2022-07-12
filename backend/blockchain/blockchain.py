112c5a33ce93bb33b1f91b123d2cac97f0bfd8a2
Litv1n <litv1n.true@gmail.com>
2022-07-12 22:10:51 +0300

    Add chain validation feature. Add tests for new feature.

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -6,26 +6,43 @@ class Blockchain:
     Blochain: a public ledger of transactions.
     Implemented as a list of blocks - data sets of transactions.
     """
+
     def __init__(self):
         self.chain = [Block.genesis()]
-    
+
     def add_block(self, data):
         self.chain.append(Block.mine_block(self.chain[-1], data))
-    
+
     def __repr__(self):
-        return f'Blockchain: {self.chain}'
+        return f"Blockchain: {self.chain}"
+
+    @staticmethod
+    def is_valid_chain(chain):
+        """
+        Validate the incoming chain.
+        Enforce the following rules of the blockchain:
+          - the chain must start with the genesis block
+          - block must be formatted correctly
+        """
+        if chain[0] != Block.genesis():
+            raise Exception("The genesis block must be valid.")
+
+        for i in range(1, len(chain)):
+            block = chain[i]
+            last_block = chain[i - 1]
+            Block.is_valid_block(last_block, block)
 
 
 def main():
     blockchain = Blockchain()
-    blockchain.add_block('one')
-    blockchain.add_block('two')
-    blockchain.add_block('three')
+    blockchain.add_block("one")
+    blockchain.add_block("two")
+    blockchain.add_block("three")
 
     print(blockchain)
 
-    print(f'blockchain.py __name__: {__name__}')
+    print(f"blockchain.py __name__: {__name__}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
