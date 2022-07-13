cc6cb0435c25473fd267a9f549684964484cc248
Litv1n <litv1n.true@gmail.com>
2022-07-13 22:10:32 +0300

    Add mine block API endpoint.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,9 +6,6 @@ from backend.blockchain.blockchain import Blockchain
 app = Flask(__name__)
 blockchain = Blockchain()
 
-for i in range(3):
-    blockchain.add_block(i)
-
 
 @app.route("/")
 def route_default():
@@ -18,6 +15,15 @@ def route_default():
 @app.route("/blockchain")
 def route_blockchain():
     return jsonify(blockchain.to_json())
+
+
+@app.route("/blockchain/mine")
+def route_blockchain_mine():
+    transaction_data = "stubbed_transaction_data"
+
+    blockchain.add_block(transaction_data)
+
+    return jsonify(blockchain.chain[-1].to_json())
 
 
 app.run()
