2cd5c0b7add7394aa96392ba40142e745d297cd9
Litv1n <litv1n.true@gmail.com>
2022-07-14 22:50:28 +0300

    Add PubSub class.

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -1,35 +1,55 @@
 import time
+import os
+from dotenv import load_dotenv
 
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
 
 
-subscribe_key = "sub-c-4ed05400-6121-49f3-bd1d-6a52441a6ae6"
-publish_key = "pub-c-e69dccee-46b5-40af-b359-77f6ab9c97c4"
+load_dotenv()
+
+SUBSCRIBE_KEY = os.getenv("SUBSCRIBE_KEY")
+PUBLISH_KEY = os.getenv("PUBLISH_KEY")
 
 pnconfig = PNConfiguration()
-pnconfig.subscribe_key = subscribe_key
-pnconfig.publish_key = publish_key
-pubnub = PubNub(pnconfig)
+pnconfig.subscribe_key = SUBSCRIBE_KEY
+pnconfig.publish_key = PUBLISH_KEY
 
 TEST_CHANNEL = "TEST_CHANNEL"
-
-pubnub.subscribe().channels([TEST_CHANNEL]).execute()
 
 
 class Listener(SubscribeCallback):
     def message(self, pubnub, message_object):
-        print(f"\n-- Incoming message_object: {message_object}")
+        print(
+            f"\n-- Channel: {message_object.channel} | Message: {message_object.message}"
+        )
 
 
-pubnub.add_listener(Listener())
+class PubSub:
+    """
+    Handles the publish/subscribe layer of the application.
+    Provides communication between the condes of the blockchain network.
+    """
+
+    def __init__(self):
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+        self.pubnub.add_listener(Listener())
+
+    def publish(self, channel, message):
+        """
+        Publish the message object to the channel.
+        """
+        self.pubnub.publish().channel(channel).message(message).sync()
 
 
 def main():
+    pubsub = PubSub()
+
     time.sleep(1)
 
-    pubnub.publish().channel(TEST_CHANNEL).message({"foo": "bar"}).sync()
+    pubsub.publish(TEST_CHANNEL, {"foo": "bar"})
 
 
 if __name__ == "__main__":
