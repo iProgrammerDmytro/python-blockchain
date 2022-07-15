23301e5c427f5838d7e0744428949232f8ceb7e7
Litv1n <litv1n.true@gmail.com>
2022-07-15 23:04:11 +0300

    Add received blocks fetature.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,7 @@ from backend.pubsub import PubSub
 
 app = Flask(__name__)
 blockchain = Blockchain()
-pubsub = PubSub()
+pubsub = PubSub(blockchain)
 
 
 @app.route("/")
--- backend/pubsub.py
+++ backend/pubsub.py
@@ -6,6 +6,8 @@ from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
 
+from backend.blockchain.block import Block
+
 
 load_dotenv()
 
@@ -21,10 +23,24 @@ pnconfig.publish_key = PUBLISH_KEY
 
 
 class Listener(SubscribeCallback):
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
     def message(self, pubnub, message_object):
         print(
             f"\n-- Channel: {message_object.channel} | Message: {message_object.message}"
         )
+
+        if message_object.channel == CHANNELS["BLOCK"]:
+            block = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
+
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print("\n -- Successfully replaced the local chain.")
+            except Exception as e:
+                print(f"\n -- Did not replace chain: {e}")
 
 
 class PubSub:
@@ -33,10 +49,10 @@ class PubSub:
     Provides communication between the condes of the blockchain network.
     """
 
-    def __init__(self):
+    def __init__(self, blockchain):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener())
+        self.pubnub.add_listener(Listener(blockchain))
 
     def publish(self, channel, message):
         """
