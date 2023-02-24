ca53b25282db31ce50c66c5e06250e968e57e9af
Dmytro Lytvynenko <litv1n.true@gmail.com>
2023-02-24 22:43:23 +0200

    mine transactions frontend

--- frontend/src/components/TransactionPool.js
+++ frontend/src/components/TransactionPool.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 import Transaction from "./Transaction";
 import { API_BASE_URL, SECONDS_JS } from "../config";
+import history from '../history';
 
 const POOL_INTERVAL = 10 * SECONDS_JS;
 
@@ -26,6 +28,15 @@ function TransactionPool() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const fetchMineBlock = () => {
+    fetch(`${API_BASE_URL}/blockchain/mine`)
+      .then(() => {
+        alert("Success");
+
+        history.push('/blockchain')
+      })
+  }
+
   return (
     <div className="TransactionPool">
       <Link to="/">Home</Link>
@@ -39,6 +50,13 @@ function TransactionPool() {
           </div>
         ))
       }
+      <hr />
+      <Button
+        variant="danger"
+        onClick={fetchMineBlock}
+      >
+        Mine a block of these transactions
+      </Button>
     </div>
   )
 }
